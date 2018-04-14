038d96c3b8955fdef6cbcf699e2afafe2537b751
Tina Giska <tina.tylicka@gmail.com>
2018-04-14 22:49:27 +0200

    Create process

---
+++ js/program.js
@@ -0,0 +1,7 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    process.stdout.write(input);
+});
