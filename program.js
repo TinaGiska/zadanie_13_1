056eb984a19be380e41f111ff965c48a768bc25c
Tina Giska <tina.tylicka@gmail.com>
2018-04-15 14:08:16 +0200

    Update files

--- program.js
+++ program.js
@@ -1,7 +1,7 @@
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    
     var input = process.stdin.read();
     if(input !== null) {
         var instruction = input.toString().trim();
