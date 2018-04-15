d6bb313e407aa2af3e29a24f8be6ecc71539dbc0
Tina Giska <tina.tylicka@gmail.com>
2018-04-15 13:05:43 +0200

    Update index.js

--- js/program.js
+++ js/program.js
@@ -3,5 +3,13 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
-    process.stdout.write(input);
+    if(input !== null) {
+        var instruction = input.toString().trim();
+        if (instruction === '/exit') {
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+        } else {
+            process.stderr.write('Wrong instruction!');
+        }
 });
+}
