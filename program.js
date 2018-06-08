b35d87ce46b9aebff0b1e5658ee53062e930b875
Tina Giska <tina.tylicka@gmail.com>
2018-06-08 21:17:42 +0200

    Update task

--- program.js
+++ program.js
@@ -1,7 +1,7 @@
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
-    
+
     var input = process.stdin.read();
     if(input !== null) {
         var instruction = input.toString().trim();
@@ -10,6 +10,7 @@ process.stdin.on('readable', function() {
             process.exit();
         } else {
             process.stderr.write('Wrong instruction!');
+
         }
-});
 }
+});
